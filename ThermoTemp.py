d5f381c4b20d4c0f9fb01e7f9ada7c54aad70f6d
Sigmund Andreas Birkeland <sigmundabirkeland@gmail.com>
2017-12-03 14:52:29 +0100

    test of Thermotemp

--- ThermoTemp.py
+++ ThermoTemp.py
@@ -1,11 +1,30 @@
 import os
 import time
+import sys
+import RPi.GPIO as GPIO
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
 
-temp_sensor = '/sys/bus/w1/devices/28-0115827775ff/w1_slave'
+def init():
+    #Temperature sensor initialization
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
+    temp_sensor = '/sys/bus/w1/devices/28-0115827775ff/w1_slave'
+    #Send rf signal initialization
+    a_on =  '10011001101010100110101001011001100101100101010101101001101010101'
+    a_off = '10011001101010100110101001011001100101100101010101101010101010101'
+    b_on =  '10011001101010100110101001011001100101100101010101101001101010011'
+    b_off = '10011001101010100110101001011001100101100101010101101010101010011'
+    c_on =  '10011001101010100110101001011001100101100101010101101001101001101'
+    c_off = '10011001101010100110101001011001100101100101010101101010101001101'
 
+    low_delay = 0.00025
+    high_delay = 0.00025
+    zero_delay = 0.001
+    long_delay = 0.01
+    start_delay = 0.00275
+    NUM_ATTEMPTS = 5
+    TRANSMIT_PIN = 23
+    
 def temp_raw():
 
     f = open(temp_sensor, 'r')
@@ -24,8 +43,41 @@ def read_temp():
     if temp_output != -1:
         temp_string = lines[1].strip()[temp_output+2:]
         temp_c = float(temp_string) / 1000.0
-        return temp_c, 
+        return temp_c
 
+def transmit_code(code):
+    '''Transmit a chosen code string using the GPIO transmitter'''
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(TRANSMIT_PIN, GPIO.OUT)
+    #TODO must set up start signal
+    for t in range(NUM_ATTEMPTS):
+        GPIO.output(TRANSMIT_PIN,1)
+        time.sleep(high_delay)
+        GPIO.output(TRANSMIT_PIN,0)
+        time.sleep(start_delay)
+        for i in code:
+            if i == '1':
+                GPIO.output(TRANSMIT_PIN, 1)
+                time.sleep(high_delay)
+                GPIO.output(TRANSMIT_PIN, 0)
+                time.sleep(low_delay)
+            elif i == '0':
+                GPIO.output(TRANSMIT_PIN, 1)
+                time.sleep(high_delay)
+                GPIO.output(TRANSMIT_PIN, 0)
+                time.sleep(zero_delay)
+            else:
+                continue
+        GPIO.output(TRANSMIT_PIN, 0)
+        time.sleep(long_delay)
+    GPIO.cleanup()
+
+init()
 while True:
     print(read_temp())
+    if read_temp() > 21:
+        transmit_code(a_off)
+    else if read_temp() < 21:
+        transmit_code(a_on)
     time.sleep(10)
+    
