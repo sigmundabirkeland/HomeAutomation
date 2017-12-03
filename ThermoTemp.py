0371eef2328a237feb567080761f29477e965e8b
Sigmund Andreas Birkeland <sigmundabirkeland@gmail.com>
2017-12-03 23:31:03 +0100

    Kanskje det gjorde susen?

--- ThermoTemp.py
+++ ThermoTemp.py
@@ -111,27 +111,27 @@ work = 16
 home = True
 
 with open("living_room_temp.csv", "a") as log:
-while True:
-    temp = read_temp()
-    if getMode() == 0:
-        thermo = normal
-        print("normal mode. Thermo = ", thermo, "Temp = ", temp)
-    elif getMode() == 1:
-        thermo = night
-        print("night mode. Thermo = ", thermo, "Temp = ", temp)
-    else:
-        thermo = work
-        print("work mode. Thermo = ", thermo, "Temp = ", temp)
-    if temp > thermo + 0.25:
-        transmit_code(a_off)
-        log.write("{0},{1},{2}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp),"off"))
-    if temp < thermo - 0.25:
-        transmit_code(a_on)
-        log.write("{0},{1},{2}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp),"on"))
-    else:
-        log.write("{0},{1},{2}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp),"unchanched"))
-    GPIO.cleanup()
-    time.sleep(300)
+    while True:
+        temp = read_temp()
+        if getMode() == 0:
+            thermo = normal
+            print("normal mode. Thermo = ", thermo, "Temp = ", temp)
+        elif getMode() == 1:
+            thermo = night
+            print("night mode. Thermo = ", thermo, "Temp = ", temp)
+        else:
+            thermo = work
+            print("work mode. Thermo = ", thermo, "Temp = ", temp)
+        if temp > thermo + 0.25:
+            transmit_code(a_off)
+            log.write("{0},{1},{2}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp),"off"))
+        if temp < thermo - 0.25:
+            transmit_code(a_on)
+            log.write("{0},{1},{2}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp),"on"))
+        else:
+            log.write("{0},{1},{2}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp),"unchanched"))
+        GPIO.cleanup()
+        time.sleep(300)
 #end program cleanly
 except KeyboardInterrupt:
     GPIO.cleanup()
