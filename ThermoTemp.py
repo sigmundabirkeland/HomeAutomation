e3548630b6de56bc06504c64a08be298868b6ffa
Sigmund Birkeland <sigmundabirkeland@gmail.com>
2018-09-26 18:07:57 +0200

    Updated with working LCD

--- ThermoTemp.py
+++ ThermoTemp.py
@@ -7,12 +7,12 @@ import RPi.GPIO as GPIO
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import I2C_LCD_driver
 
 def init():
     #Temperature sensor initialization
     os.system('modprobe w1-gpio')
     os.system('modprobe w1-therm')
-
 def temp_raw():
 
     f = open(temp_sensor, 'r')
@@ -62,7 +62,7 @@ def getMode():
     weekday = int(time.strftime("%w"))
     hour = float(time.strftime("%H"))
     if weekday > 0 and weekday < 5:
-        if hour >= 22 or hour < 6:
+        if hour >= 23 or hour < 6:
             return(1)
         if home == True:
             return(0)
@@ -71,7 +71,7 @@ def getMode():
         else:
             return(0)
     if weekday == 5:
-        if hour >= 23 or hour < 6:
+        if hour >= 24 or hour < 6:
             return(1)
         if home == True:
             return(0)
@@ -85,12 +85,13 @@ def getMode():
         else:
             return(0)
     if weekday == 0:
-        if hour >= 22 or hour < 8:
+        if hour >= 23 or hour < 8:
             return(1)
         else:
             return(0)
 
 def plotGraph(temp,dateChanged,status,reset_time):
+    dateChanged = 0
     if dateChanged == 1:
 	plt.clf()
 	plt.scatter(x,y)
@@ -101,8 +102,8 @@ def plotGraph(temp,dateChanged,status,reset_time):
 	y[:] = []
 	reset_time = time.time()
    	dateChanged = 0
-    y.append(temp)
-    x.append(time.time() - reset_time)
+    #y.append(temp)
+    #x.append(time.time() - reset_time)
 
 
 init()
@@ -114,6 +115,8 @@ b_on =  '10011001101010100110101001011001100101100101010101101001101010011'
 b_off = '10011001101010100110101001011001100101100101010101101010101010011'
 c_on =  '10011001101010100110101001011001100101100101010101101001101001101'
 c_off = '10011001101010100110101001011001100101100101010101101010101001101'
+
+mylcd = I2C_LCD_driver.lcd()
 
 low_delay = 0.00025
 high_delay = 0.00025
@@ -127,7 +130,7 @@ TRANSMIT_PIN = 23
 x = []
 y = []
 
-normal = 21
+normal = 22
 night = 16
 work = 16
 status = 0
@@ -135,38 +138,47 @@ home = True
 date = datetime.datetime.now().day
 dateChanged = 0
 reset_time = time.time()
+thermo = 0
 
 try:
     while True:
     	with open("/home/pi/HomeAutomation/living_room_temp.csv", "a") as log:
             temp = read_temp()
-            if getMode() == 0:
+	    mylcd.lcd_clear()
+	    mylcd.lcd_display_string("Temp: %d%sC" % (temp, chr(223)),1)
+	    mylcd.lcd_display_string("Target: %d%sC" % (thermo, chr(223)),2)
+	    if getMode() == 0:
                 thermo = normal
-		#print("normal mode. Thermo = ", thermo, "Temp = ", temp)
+		#mylcd.lcd_clear()
+		#mylcd.lcd_display_string("Temp: %d%sC" % (temp, chr(223)),1)
+		mylcd.lcd_display_string("Normal mode",4)
             elif getMode() == 1:
                 thermo = night
-                #print("night mode. Thermo = ", thermo, "Temp = ", temp)
+                mylcd.lcd_display_string("Night mode",4)
+		#print("night mode. Thermo = ", thermo, "Temp = ", temp)
             else:
                 thermo = work
+		mylcd.lcd_display_string("Work mode",4)
                 #print("work mode. Thermo = ", thermo, "Temp = ", temp)
             if temp > thermo + 0.25:
                 transmit_code(a_off)
 		transmit_code(c_off)
+		mylcd.lcd_display_string("Status: Off",3)
 		#print("Thermostat off, temp = ",temp)
                 if status == 1:
 		    log.write("{0},{1},{2}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str(temp),"0"))
             	    status = 0
 		else:
 		    log.write("{0},{1},{2}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str(temp),"0"))
-		
 	    elif temp < thermo - 0.25:
                 transmit_code(a_on)
 		transmit_code(c_on)
+		mylcd.lcd_display_string("Status: On",3)
                 if status == 0:
 		    log.write("{0},{1},{2}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str(temp),"1"))
 		    status = 1
 		else:
-		    log.write("{0},{1},{2}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str(temp),"1"))            	
+		    log.write("{0},{1},{2}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str(temp),"1"))
 		#print("Thermostat on, temp = ",temp)
 	    else:
 		#print("Thermostat unchanged, temp= ", temp)
@@ -178,9 +190,10 @@ try:
                     dateChanged = True
                     date = datetime.datetime.now().day
 	    plotGraph(temp,dateChanged,status,reset_time)
-	time.sleep(300)
+	time.sleep(30)
 #end program cleanly
 except KeyboardInterrupt:
     #GPIO.cleanup()
+    mylcd.lcd_clear()
     print "done"
 
